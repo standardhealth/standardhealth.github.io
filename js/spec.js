619f1fbe64c05f32c7be105adf51266eb640b717
Dylan Phelan <dtphelan1@gmail.com>
2016-11-21 17:11:26 -0500

    SPEC now underlined on all spec viewer pages, edited button on HOME

--- js/spec.js
+++ js/spec.js
@@ -29,52 +29,17 @@ $('#site-menu li a').on('click', function(){
     $("#site-menu").collapse('hide');
 });
 
-function setActive(str) { 
+function setActive() { 
     $("nav li")
         .removeClass('active')
         .find('a')
-        .filter (function (elem) { return this.innerHTML == str})
+        .filter (function (elem) { return this.innerHTML == "SPEC"})
         .parent()
         .addClass('active');
     return;
 }
 
-function parseSectionActive() {
-    switch ($(window)[0].location.pathname) { 
-        case ("/"):
-            switch($(window)[0].location.hash) { 
-                case("#home"): 
-                    setActive("HOME");
-                    break;
-                case("#about"): 
-                    setActive("ABOUT");
-                    break;
-                case("#contact"): 
-                    setActive("CONTACT");
-                    break;
-                default: 
-                    alert('on a new section of homepage: ' + $window[0].location.hash);
-                    break;
-            } 
-            break; 
-        case ("/evidence.html"):
-            setActive("EVIDENCE");
-            break;
-        case ("/spec.html"):
-            setActive("SPEC");
-            break;
-        case ("/specviewer.html"):
-            console.log("Spec viewer isn't in navbar -- skip");
-            break;
-        case ("/faq.html"):
-            setActive("FAQ");
-            break;
-        default:
-            alert('on a new section of homepage: ' + $(window)[0].location.hash);
-            break;
-    }
-}
-$(document).ready(parseSectionActive);
+$(document).ready(setActive);
 
 $(window).scroll(function(event) {
         updateActive();
