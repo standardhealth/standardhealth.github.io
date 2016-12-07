63fc5a44704abfd7fdc9d699a8cf7079465f929b
Dylan Phelan <dtphelan1@gmail.com>
2016-12-07 14:23:50 -0500

    refactored home.ks

--- js/home.js
+++ js/home.js
@@ -1,6 +1,27 @@
 // Set active section of homepage based on scrolling location
 $(window).scroll(function(event) {
-    if ($(window)[0].location.pathname == "/") { 
             updateActive();
-    }
 }); 
+// Update the active location in the nav bar
+function updateActive() {
+    let linkTops = [];
+    const wTop     = $(window).scrollTop();
+    const rangeTop = 5;
+    $('nav').find('.scroll a').each(function(){
+        linkTops.push($(this.hash).offset().top - 200);
+    });
+    if ($(window).scrollTop() + $(window).height() + 15 >= $(document).height()) {
+        $("nav li.scroll")
+            .removeClass('active')
+            .eq(linkTops.length -1).addClass('active');
+        
+    } else {
+        $.each( linkTops, function(i) {
+            if ( wTop > linkTops[i] - rangeTop ){
+                $('nav li.scroll')
+                    .removeClass('active')     // Drop any active elems (of which there are one)
+                    .eq(i).addClass('active'); // Add active to the current element                 
+            }
+        });
+    }
+};
