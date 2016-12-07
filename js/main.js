8f321cd75d111926d88702144e24b16cd0d7d6cb
Dylan Phelan <dtphelan1@gmail.com>
2016-12-07 15:02:21 -0500

    Trying to revert again"

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $('#site-menu a').on('touchend', function(e) {
         return;
     } else {
         $("#site-menu").collapse('hide');
-    }   
+    }
 });
 // Close then menu when clicking off of it
 $(document).ready(function () {
@@ -64,7 +64,7 @@ $(document).ready(parseSectionActive);
 // Set active section of homepage based on scrolling location
 $(window).scroll(function(event) {
     if (window.location.pathname == "/") { 
-            updateActive();
+        updateActive();
     }
 }); 
 function parseSectionActive() {
@@ -113,27 +113,4 @@ function setActive(str) {
         .parent()
         .addClass('active');
     return;
-};
-// Update the active location in the nav bar
-function updateActive() {
-    let linkTops = [];
-    const wTop     = $(window).scrollTop();
-    const rangeTop = 5;
-    $('nav').find('.scroll a').each(function(){
-        linkTops.push($(this.hash).offset().top - 200);
-    });
-    if ($(window).scrollTop() + $(window).height() + 15 >= $(document).height()) {
-        $("nav li.scroll")
-            .removeClass('active')
-            .eq(linkTops.length -1).addClass('active');
-        
-    } else {
-        $.each( linkTops, function(i) {
-            if ( wTop > linkTops[i] - rangeTop ){
-                $('nav li.scroll')
-                    .removeClass('active')     // Drop any active elems (of which there are one)
-                    .eq(i).addClass('active'); // Add active to the current element                 
-            }
-        });
-    }
 };
