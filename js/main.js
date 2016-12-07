0accd995ce44245b490402a28f105ff6663074c6
Dylan Phelan <dtphelan1@gmail.com>
2016-12-07 17:37:18 -0500

    trying to debug clickover obj

--- js/main.js
+++ js/main.js
@@ -74,12 +74,14 @@ $(document).ready(function () {
     });
 });
 
-// $(document).ready(function () {
-//     $(document).on('touchend', function (event) {
-//         const clickover = $(event.target);
-//         const _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse in");
-//         if (_opened && !clickover.hasClass("navbar-toggle") ) {
-//             $("button.navbar-toggle").click();
-//         }
-//     });
-// });
+$(document).ready(function () {
+    $(document).on('touchend', function (event) {
+        const clickover = $(event.target);
+        const _opened = $(".navbar-collapse").hasClass("navbar-collapse collapse in");
+        if (_opened && !clickover.hasClass("navbar-toggle") && !clickover.hasClass("dropdown-toggle")) {
+            $("button.navbar-toggle").click();
+        } else { 
+            alert(clickover.text())
+        }
+    });
+});
