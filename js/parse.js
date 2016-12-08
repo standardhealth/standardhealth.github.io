85910c5039fe9cda84f492750e42be1855c26638
Dylan Phelan <dtphelan1@gmail.com>
2016-12-08 13:27:09 -0500

    Changed SHR to modt recent; FAQ updated; Evidence round one

---
+++ js/parse.js
@@ -0,0 +1,53 @@
+// Set Active functionality for elements in nav bar
+//
+// Set active element in list based on current page
+$(document).ready(parseSectionActive);
+// Parse the active section based on current location
+function parseSectionActive() {
+    switch (window.location.pathname.split('/')[1]) { 
+        case (""):
+            switch(window.location.hash) { 
+                case("#home"): 
+                    setActive("HOME");
+                    break;
+                case("#about"): 
+                    setActive("ABOUT");
+                    break;
+                case("#contact"): 
+                    setActive("CONTACT");
+                    break;
+                case(""):
+                    setActive("HOME");
+                    break;    
+                default: 
+                    alert('on a new section of homepage: ' + window.location.hash);
+                    break;
+            } 
+            break; 
+        case ("evidence-nav.html"):
+            setActive("EVIDENCE");
+            break;
+        case ("spec.html"):
+            setActive("SPEC");
+            break;
+        case ("shr"):
+            setActive("SPEC");
+            break;
+        case ("faq.html"):
+            setActive("FAQ");
+            break;
+        default:
+            alert('on a new section of page: ' + window.location.pathname.split('/')[1]);
+            break;
+    }
+}
+// Set active location based on given text
+function setActive(str) { 
+    $("nav li")
+        .removeClass('active')
+        .find('a')
+        .filter (function (elem) { return this.innerText == str})
+        .parent()
+        .addClass('active');
+    return;
+};
