9a68894752aa1deab5535ab04d7462a60a8ac7fe
Dylan Phelan <dtphelan1@gmail.com>
2017-01-23 13:55:15 -0500

    snackbar text edit

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ function snackbarGeneration() {
     var d = new Date();
     var himssEnds = new Date("2/24/2017");
     var options =  {
-        content: "<p>We're going to be at HIMSS, February 22, 2017! <a href='http://www.himssconference.org/session/standard-health-records-legal-and-policy-challenges'>Click here</a> for info on our 4 o'clock presentation on \"Standard Health Records: The Legal and Policy Challenges.\"</p>", // text of the snackbar 
+        content: "<p>We're going to be at HIMSS, February 22, 2017! <a href='http://www.himssconference.org/session/standard-health-records-legal-and-policy-challenges'>Click here</a> for details on our presentation, \"Standard Health Records: The Legal and Policy Challenges.\"</p>", // text of the snackbar 
 
         htmlAllowed: true, // allows HTML as content value
         timeout: 10000 // time in milliseconds after the snackbar autohides, 0 is disabled
