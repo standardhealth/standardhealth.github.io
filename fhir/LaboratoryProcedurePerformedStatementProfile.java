ea5ec38a10618b6bc64d769863d431779d1470e8
markkramerus <mkramer@mitre.org>
2019-04-24 09:09:32 -0400

    Eliminated files unnecessary for static site

--- fhir/LaboratoryProcedurePerformedStatementProfile.java
+++
@@ -1,7 +0,0 @@
-package http://standardhealthrecord.org/fhir/ImplementationGuide/1;
-
-import org.hl7.fhir.r4.model.ProfilingWrapper;
-
-public class LaboratoryProcedurePerformedStatementProfile {
-
-}
